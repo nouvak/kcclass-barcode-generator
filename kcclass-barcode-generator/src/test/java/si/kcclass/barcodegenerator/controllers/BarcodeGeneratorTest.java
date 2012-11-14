c0b590d028a4945cc68eea778d349bacdb0c486e
Marko Novak <nouvak@gmail.com>
2012-11-14 15:32:54 +0100

    Add created unit test file for the barcode generator.

---
+++ kcclass-barcode-generator/src/test/java/si/kcclass/barcodegenerator/controllers/BarcodeGeneratorTest.java
@@ -0,0 +1,24 @@
+package si.kcclass.barcodegenerator.controllers;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class BarcodeGeneratorTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
