5515066ce2c6b2c247ab2d003d1ef3f4e2fb3451
Marko Novak <nouvak@gmail.com>
2012-11-14 15:19:45 +0100

    Added the project to the repository.

---
+++ kcclass-barcode-generator/src/main/java/si/kcclass/barcodegenerator/controllers/BarcodeGenerator.java
@@ -0,0 +1,28 @@
+package si.kcclass.barcodegenerator.controllers;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@RequestMapping("/barcodegenerator/**")
+@Controller
+public class BarcodeGenerator {
+
+    @RequestMapping(method = RequestMethod.POST, value = "{id}")
+    public void post(@PathVariable Long id, ModelMap modelMap, HttpServletRequest request, HttpServletResponse response) {
+    }
+
+    @RequestMapping
+    public String index() {
+        return "barcodegenerator/index";
+    }
+    
+    @RequestMapping(value="generate/strBarcode", method = RequestMethod.GET)
+    public void generate(@PathVariable String strBarcode) {
+    	
+    }
+}
